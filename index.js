b26afa4176b64694c8e01860280153941de6c55d
Jake(Jaehak Song) <jake@heybit.io>
2024-02-04 04:24:07 +0900

    Fix command is undefined error

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const showPlayers = async () => {
           console.log(`No players for 20 minutes, shutting down server. noPlayerFrom: ${noPlayerFrom}, now: ${Date.now()}`)
           const channel = client.channels.cache.get(config.notice_channel);
           channel.send("플레이어가 20분 동안 없어서 서버를 종료합니다. 플레이 하시려면 /start 명령어로 서버를 다시 시작해주세요.");
-          const stdout = execSync(config.server_shutdown_command);
+          const stdout = execSync(config.server_stop_command);
           console.log(stdout.toString());
         }
 
